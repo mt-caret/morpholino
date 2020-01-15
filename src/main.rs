d0199137cbacb2f8608fe0e9d1fbf839e32b1626
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2020-01-16 08:42:01 +0900

    implement morpheme boundary detection

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,31 @@ struct Opt {
     out_path: PathBuf,
 }
 
+fn cosine_similarity(a: &[f64], b: &[f64]) -> f64 {
+    assert_eq!(a.len(), b.len());
+    a.iter().zip(b.iter()).map(|(x, y)| x * y).sum::<f64>()
+        / a.iter().map(|x| x * x).sum::<f64>().sqrt()
+        / b.iter().map(|x| x * x).sum::<f64>().sqrt()
+}
+
+fn detect_morpheme_boundaries(
+    word: &str,
+    embeddings: &HashMap<String, Vec<f64>>,
+    threshold: f64,
+) -> Option<Vec<usize>> {
+    let mut current = embeddings.get(word)?;
+    let mut boundary_indices = Vec::new();
+    for boundary_index in (1..word.len()).rev() {
+        if let Some(new) = embeddings.get(&word[0..boundary_index]) {
+            if cosine_similarity(current, new) > threshold {
+                boundary_indices.push(boundary_index);
+                current = new;
+            }
+        }
+    }
+    Some(boundary_indices)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
     let buffer = fs::read_to_string(opt.path)?;
@@ -29,20 +54,27 @@ fn main() -> Result<(), Box<dyn Error>> {
     let d = tokens[1].parse::<usize>()?;
     println!("n = {}, d = {}", n, d);
 
-    let embeddings: HashMap<String, Vec<f32>> = buffer[first_line_offset + 1..]
+    let embeddings: HashMap<String, Vec<f64>> = buffer[first_line_offset + 1..]
         .par_lines()
         .map(|line| {
             let mut tokens = line.trim().split(' ');
             let word = tokens.next().expect("No word found").to_string();
 
-            let vector: Result<Vec<f32>, _> = tokens.map(|val| val.parse::<f32>()).collect();
+            let vector: Result<Vec<f64>, _> = tokens.map(|val| val.parse::<f64>()).collect();
             let vector = vector.expect("Parse failure");
+            assert_eq!(vector.len(), d);
             let mut m = HashMap::new();
             m.insert(word, vector);
             m
         })
         .reduce(|| HashMap::new(), |a, b| a.into_iter().chain(b).collect());
+    assert_eq!(embeddings.len(), n);
     println!("Done reading.");
+
+    for key in embeddings.keys().take(10) {
+        let boundaries = detect_morpheme_boundaries(key, &embeddings, 0.25);
+        println!("{}: {:?}", key, boundaries);
+    }
 
     bincode::serialize_into(out_file, &embeddings)?;
     println!("Done writing.");
